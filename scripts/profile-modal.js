e4af0de540c10a0d47249b490a3be004862306a8
sergioduranmi <sergioduran182@gmail.com>
2024-08-04 16:01:48 -0500

    refactor: generalizar estructura modal
    Iteración 2, template funcionando modal de perfil

--- scripts/profile-modal.js
+++ scripts/profile-modal.js
@@ -1,45 +1,57 @@
 // Constantes y variables
 // constantes de modal 3:24 funcionando
-const modalTemplate = document.querySelector("#modal-template").content;
-const modal = document.querySelector("#Modal");
-const closeButton = document.querySelector("#closeModalBtn");
-const ModalForm = document.querySelector(".modal__form");
-const inputOne = document.querySelector("#inputOne");
-const inputTwo = document.querySelector("#inputTwo");
-const submitBtn = document.querySelector(".modal__submit-btn");
 
 // Constantes de perfil
 const openProfileButton = document.querySelector(".edit-profile-button");
 const profileName = document.querySelector(".user-profile__name");
 const profileJob = document.querySelector(".user-profile__role");
 
-// Función para abrir y cerrar el modal de perfil
+// Función
 function actionModal() {
-  modal.classList.toggle("modal_opened");
-}
+  // constante en modal
+  const modalTemplate = document.querySelector("#modal-template").content;
+  const modalElement = modalTemplate.querySelector(".modal").cloneNode(true);
+  const closeButton = modalElement.querySelector("#closeModalBtn");
+  const ModalForm = modalElement.querySelector(".modal__form");
+  const inputOne = modalElement.querySelector("#inputOne");
+  const inputTwo = modalElement.querySelector("#inputTwo");
+  const submitBtn = modalElement.querySelector(".modal__submit-btn");
 
-// Función para enviar el formulario del modal de perfil
-function submitModal(event) {
-  event.preventDefault();
+  // Añadir el modal al DOM
+  document.body.appendChild(modalElement);
+  modalElement.classList.add("modal_opened");
 
-  // Aquí puedes añadir el código para actualizar el contenido del perfil del usuario, si es necesario.
-  profileName.textContent = inputOne.value;
-  profileJob.textContent = inputTwo.value;
-}
-// Función para cambiar color botón submit
-function changeColor() {
-  if (inputOne.value.trim() !== "" && inputTwo.value.trim() !== "") {
-    submitBtn.style.backgroundColor = "#000";
-    submitBtn.style.color = "#fff";
-  } else {
-    submitBtn.style.backgroundColor = "";
-    submitBtn.style.color = "";
+  // Cambiar color del botón submit
+  function changeColor() {
+    if (inputOne.value.trim() !== "" && inputTwo.value.trim() !== "") {
+      submitBtn.style.backgroundColor = "#000";
+      submitBtn.style.color = "#fff";
+    } else {
+      submitBtn.style.backgroundColor = "";
+      submitBtn.style.color = "";
+    }
   }
+
+  // Función para enviar el formulario del modal de perfil
+  function submitModal(event) {
+    event.preventDefault();
+
+    // Aquí puedes añadir el código para actualizar el contenido del perfil del usuario, si es necesario.
+    profileName.textContent = inputOne.value;
+    profileJob.textContent = inputTwo.value;
+  }
+
+  // Función para cerrar el modal
+  function closeModal() {
+    document.body.removeChild(modalElement);
+  }
+  // Eventos dentro de modal
+  inputOne.addEventListener("input", changeColor);
+  inputTwo.addEventListener("input", changeColor);
+  ModalForm.addEventListener("submit", submitModal);
+  ModalForm.addEventListener("submit", closeModal);
+  closeButton.addEventListener("click", closeModal);
 }
-// Eventos
-inputOne.addEventListener("input", changeColor);
-inputTwo.addEventListener("input", changeColor);
-ModalForm.addEventListener("submit", submitModal);
+
+// Eventos del perfil
 openProfileButton.addEventListener("click", actionModal);
-closeButton.addEventListener("click", actionModal);
-ModalForm.addEventListener("submit", actionModal);
